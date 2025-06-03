209c845cf5d5f7ac54a736dbd76b4eb4abf95243
DanBrock10 <danbrock10@hotmail.ca>
2025-06-02 21:31:58 -0400

    Add selected array to store chosen words

---
+++ Desktop/Assignment1_Client_side_java/script.js
@@ -0,0 +1,9 @@
+var phrases = [
+  ["The turkey", "Dad", "The dog", "My teacher", "The elephant", "The cat", "My friend", "The chicken"],
+  ["sat on", "ate", "danced with", "saw", "doesn't like", "kissed", "ran to", "jumped on"],
+  ["a funny", "a scary", "a slimy", "a fat", "a goofy", "a barking", "a hairy", "a tiny"],
+  ["goat", "monkey", "frog", "worm", "bug", "elephant", "cat", "lion"],
+  ["on the moon", "in my soup", "on the chair", "in my spaghetti", "on the grass", "in the garage", "under the bed", "on the roof"]
+];
+
+var selected = ["", "", "", "", ""];
